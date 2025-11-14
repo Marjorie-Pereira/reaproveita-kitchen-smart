86dfa9c54314bea32ce5a018aa034e4348d19c83
Marjorie-Pereira <marjoriepereira@rede.ulbra.br>
2025-11-14 15:18:53 -0300

    fixes to work with cron

--- supabase/functions/push/index.ts
+++ supabase/functions/push/index.ts
@@ -18,29 +18,21 @@ const corsHeaders = {
 };
 
 Deno.serve(async (req) => {
+  if (req.method === "OPTIONS") {
+    return new Response("ok", { headers: corsHeaders });
+  }
+
   const payload: CronPayload = await req.json();
   console.log("🚀 ~ Deno.serve ~ payload:", payload);
 
-  // Create a Supabase client with the Auth context of the logged in user.
   const supabaseClient = createClient(
-    // Supabase API URL - env var exported by default.
-    Deno.env.get("SUPABASE_URL") ?? "",
-    // Supabase API ANON KEY - env var exported by default.
-    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
-    // Create client with Auth context of the user that called the function.
-    // This way your row-level-security (RLS) policies are applied.
-    {
-      global: {
-        headers: { Authorization: req.headers.get("Authorization")! },
-      },
-    }
+    Deno.env.get("SUPABASE_URL")!,
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!
   );
 
-  // And we can run queries in the context of our authenticated user
   const { data, error } = await supabaseClient.from("users").select("*");
   if (error) throw error;
 
-  console.log("data: ", data);
   const push_token = data[0]?.push_token;
   console.log("🚀 ~ Deno.serve ~ push_token:", push_token);
 
