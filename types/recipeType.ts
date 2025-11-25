b5caf0b9b0f79a1aa73002b62e8828706e8671e8
Marjorie-Pereira <marjoriepereira@rede.ulbra.br>
2025-11-25 03:16:13 -0300

    updated recipe type

--- types/recipeType.ts
+++ types/recipeType.ts
@@ -3,7 +3,7 @@ export type recipe = {
   receita: string;
   ingredientes: string;
   modo_preparo: string;
-  IngredientesBase: any[];
+  ingredientes_base: string[];
   link_imagem: string;
   tempo_preparo: string;
   categoria: string;
