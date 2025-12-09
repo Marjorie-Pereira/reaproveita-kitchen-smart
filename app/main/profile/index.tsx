04f967ae4bcf14732dd0e32acf57def8fc689a31
Marjorie-Pereira <marjorie300904@gmail.com>
2025-12-08 22:25:53 -0300

    style: keyboard avoid profile page

--- app/main/profile/index.tsx
+++ app/main/profile/index.tsx
@@ -10,263 +10,287 @@ import { normalizeString } from "@/utils/capitalizeString";
 import { Entypo, Feather } from "@expo/vector-icons";
 import { useFocusEffect } from "expo-router";
 import React, { useCallback, useEffect, useState } from "react";
-import { Alert, Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const Index = () => {
-  const { user } = useAuth();
-  const [userData, setUserData] = useState<userData | null>(null);
-  const [foodRestrictions, setFoodRestrictions] = useState<any[]>([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [description, setDescription] = useState("");
+    const { user } = useAuth();
+    const [userData, setUserData] = useState<userData | null>(null);
+    const [foodRestrictions, setFoodRestrictions] = useState<any[]>([]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [description, setDescription] = useState("");
 
-  const removeRestriction = async (restrictionId: number) => {
-    const { data, error } = await supabase
-      .from("RestricoesAlimentares")
-      .delete()
-      .eq("id", restrictionId);
+    const removeRestriction = async (restrictionId: number) => {
+        const { error } = await supabase
+            .from("RestricoesAlimentares")
+            .delete()
+            .eq("id", restrictionId);
 
-    if (error) throw new Error(error.message);
+        if (error) throw new Error(error.message);
 
-    fetchFoodRestrictions();
-  };
+        fetchFoodRestrictions();
+    };
 
-  const fetchUserData = async () => {
-    if (!user) return;
+    const fetchUserData = async () => {
+        if (!user) return;
 
-    const { data, error } = await supabase
-      .from("users")
-      .select("*")
-      .eq("id", user.id);
-    if (error) throw new Error(error.message);
+        const { data, error } = await supabase
+            .from("users")
+            .select("*")
+            .eq("id", user.id);
+        if (error) throw new Error(error.message);
 
-    setUserData(data[0]);
-  };
+        setUserData(data[0]);
+    };
 
-  const fetchFoodRestrictions = async () => {
-    if (!user) return;
+    const fetchFoodRestrictions = async () => {
+        if (!user) return;
 
-    const { data, error } = await supabase
-      .from("RestricoesAlimentares")
-      .select("*")
-      .eq("id_usuario", user.id);
-    if (error) throw new Error(error.message);
+        const { data, error } = await supabase
+            .from("RestricoesAlimentares")
+            .select("*")
+            .eq("id_usuario", user.id);
+        if (error) throw new Error(error.message);
 
-   
+        setFoodRestrictions(data);
+    };
 
-    setFoodRestrictions(data);
-  };
+    const addRestriction = async () => {
+        if (!user || description.trim() === "") return;
+        const normalizedDesc = normalizeString(description);
 
-  const addRestriction = async () => {
-    if (!user || description.trim() === '') return;
-    const normalizedDesc = normalizeString(description)
+        const { data, error } = await supabase
+            .from("RestricoesAlimentares")
+            .insert({ descricao: normalizedDesc, id_usuario: user.id });
 
-    const { data, error } = await supabase
-      .from("RestricoesAlimentares")
-      .insert({ descricao: normalizedDesc, id_usuario: user.id });
+        if (error) throw new Error(error.message);
 
-    if (error) throw new Error(error.message);
+        setDescription("");
 
-  
-    setDescription("");
+        fetchFoodRestrictions();
+    };
 
-    fetchFoodRestrictions();
-  };
+    useEffect(() => {}, [userData, foodRestrictions]);
 
-  useEffect(() => {
-  }, [userData, foodRestrictions]);
+    useEffect(() => {
+        fetchUserData();
+    }, [user, foodRestrictions]);
 
-  useEffect(() => {
-    fetchUserData();
-  }, [user, foodRestrictions]);
+    const updateUserData = async (id: string, newData: userData) => {
+        const { error } = await supabase
+            .from("users")
+            .update({
+                first_name: newData.first_name,
+                profile_pic: newData.profile_pic,
+                phone: newData.phone,
+            })
+            .eq("id", id);
 
-  const updateUserData = async (id: string, newData: userData) => {
-    const { error } = await supabase
-      .from("users")
-      .update({
-        first_name: newData.first_name,
-        profile_pic: newData.profile_pic,
-        phone: newData.phone,
-      })
-      .eq("id", id);
+        if (error) {
+            Alert.alert("Erro ao atualizar informações");
+            console.error(error);
+            return;
+        } else {
+            Alert.alert("Alterações salvas!");
+            fetchUserData();
+        }
+    };
 
-    if (error) {
-      Alert.alert("Erro ao atualizar informações");
-      console.error(error);
-      return;
-    } else {
-      Alert.alert("Alterações salvas!");
-      fetchUserData();
-    }
-  };
+    useFocusEffect(
+        useCallback(() => {
+            fetchUserData();
+            fetchFoodRestrictions();
 
-  useFocusEffect(
-    useCallback(() => {
-      fetchUserData();
-      fetchFoodRestrictions();
+            return () => {};
+        }, [])
+    );
 
-      return () => {};
-    }, [])
-  );
-
-  return (
-    <>
-      <ScrollView>
-        <View style={styles.container}>
-          <View style={styles.profileTopSection}>
-            <Image
-              resizeMode="cover"
-              source={{
-                uri: userData?.profile_pic ?? fallbackImg,
-              }}
-              style={{
-                width: 150,
-                height: 150,
-                aspectRatio: 1,
-                marginBottom: 10,
-                borderRadius: 100,
-              }}
-            />
-            <Text style={styles.userName}>{userData?.first_name}</Text>
-          </View>
-          <View style={styles.userInfoContainer}>
-            <View style={styles.userInfo}>
-              <Feather name="mail" size={20} color={COLORS.slate600} />
-              <Text style={styles.userInfoText}>{userData?.email}</Text>
-            </View>
-            <View style={styles.userInfo}>
-              <Feather name="phone" size={20} color={COLORS.slate600} />
-              <Text style={styles.userInfoText}>
-                {userData?.phone ?? "Nenhum número cadastrado"}
-              </Text>
-            </View>
-          </View>
-
-          {/* restrições */}
-          <View
-            style={{
-              ...styles.profileTopSection,
-              alignItems: "flex-start",
-            }}
-          >
-            <View>
-              <Text style={[styles.restrictionsTitle, { textAlign: "left" }]}>
-                Restrições Alimentares
-              </Text>
-              <Text style={{ color: COLORS.slate600 }}>
-                Gerencie suas restrições alimentares
-              </Text>
-            </View>
-            <View style={styles.selectedRestrictions}>
-              {foodRestrictions.map((item, index) => (
-                <RestrictionChip
-                  text={item.descricao}
-                  key={item.id}
-                  onRemove={() => removeRestriction(item.id)}
-                />
-              ))}
-            </View>
-          </View>
-          <View style={styles.newRestrictionInput}>
-            <Input
-              placeholder="Adicionar nova restrição"
-              containerStyles={{
-                flex: 1,
-              }}
-              value={description}
-              onChangeText={setDescription}
-            />
-            <Button
-              buttonStyle={{
-                width: 50,
-                height: 50,
-                borderRadius: 10,
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-              onPress={addRestriction}
+    return (
+        <>
+            <KeyboardAwareScrollView
+                style={{ flex: 1 }}
+                resetScrollToCoords={{ x: 0, y: 0 }}
+                contentContainerStyle={styles.container}
+                scrollEnabled={true}
+                enableOnAndroid={true} 
+                extraScrollHeight={180}
             >
-              <Entypo name="plus" size={20} color={COLORS.white} />
-            </Button>
-          </View>
+                <View style={styles.profileTopSection}>
+                        <Image
+                            resizeMode="cover"
+                            source={{
+                                uri: userData?.profile_pic ?? fallbackImg,
+                            }}
+                            style={{
+                                width: 150,
+                                height: 150,
+                                aspectRatio: 1,
+                                marginBottom: 10,
+                                borderRadius: 100,
+                            }}
+                        />
+                        <Text style={styles.userName}>
+                            {userData?.first_name}
+                        </Text>
+                    </View>
+                    <View style={styles.userInfoContainer}>
+                        <View style={styles.userInfo}>
+                            <Feather
+                                name="mail"
+                                size={20}
+                                color={COLORS.slate600}
+                            />
+                            <Text style={styles.userInfoText}>
+                                {userData?.email}
+                            </Text>
+                        </View>
+                        <View style={styles.userInfo}>
+                            <Feather
+                                name="phone"
+                                size={20}
+                                color={COLORS.slate600}
+                            />
+                            <Text style={styles.userInfoText}>
+                                {userData?.phone ?? "Nenhum número cadastrado"}
+                            </Text>
+                        </View>
+                    </View>
 
-          <Button
-            buttonStyle={{ marginTop: 20 }}
-            onPress={() => setIsModalOpen(true)}
-          >
-            <Text style={{ color: "white", fontWeight: "500" }}>
-              Editar Informações
-            </Text>
-          </Button>
-        </View>
-      </ScrollView>
-      <EditUserInfoModal
-        isVisible={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onSubmit={(userId, newValue) => {
-          updateUserData(userId, newValue);
-        }}
-        userData={userData!}
-      />
-    </>
-  );
+                    {/* restrições */}
+                    <View
+                        style={{
+                            ...styles.profileTopSection,
+                            alignItems: "flex-start",
+                        }}
+                    >
+                        <View>
+                            <Text
+                                style={[
+                                    styles.restrictionsTitle,
+                                    { textAlign: "left" },
+                                ]}
+                            >
+                                Restrições Alimentares
+                            </Text>
+                            <Text style={{ color: COLORS.slate600 }}>
+                                Gerencie suas restrições alimentares
+                            </Text>
+                        </View>
+                        <View style={styles.selectedRestrictions}>
+                            {foodRestrictions.map((item, index) => (
+                                <RestrictionChip
+                                    text={item.descricao}
+                                    key={item.id}
+                                    onRemove={() => removeRestriction(item.id)}
+                                />
+                            ))}
+                        </View>
+                    </View>
+                    <View style={styles.newRestrictionInput}>
+                        <Input
+                            placeholder="Adicionar nova restrição"
+                            containerStyles={{
+                                flex: 1,
+                            }}
+                            value={description}
+                            onChangeText={setDescription}
+                        />
+                        <Button
+                            buttonStyle={{
+                                width: 50,
+                                height: 50,
+                                borderRadius: 10,
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                            onPress={addRestriction}
+                        >
+                            <Entypo
+                                name="plus"
+                                size={20}
+                                color={COLORS.white}
+                            />
+                        </Button>
+                    </View>
+
+                    <Button
+                        buttonStyle={{ marginTop: 20 }}
+                        onPress={() => setIsModalOpen(true)}
+                    >
+                        <Text style={{ color: "white", fontWeight: "500" }}>
+                            Editar Informações
+                        </Text>
+                    </Button>
+            </KeyboardAwareScrollView>
+            <EditUserInfoModal
+                isVisible={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onSubmit={(userId, newValue) => {
+                    updateUserData(userId, newValue);
+                }}
+                userData={userData!}
+            />
+        </>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    borderWidth: 1,
-    marginHorizontal: 20,
-    marginTop: 20,
-    backgroundColor: "white",
-    borderColor: COLORS.slate200,
-    borderRadius: 15,
-    padding: 20,
-  },
-  profileTopSection: {
-    borderBottomWidth: 1,
-    borderBottomColor: COLORS.slate200,
-    width: "100%",
-    alignItems: "center",
-    paddingVertical: 18,
-    gap: 20,
-  },
-  userName: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  userInfoContainer: {
-    marginVertical: 20,
-    gap: 20,
-  },
-  userInfo: {
-    flexDirection: "row",
-    gap: 10,
-    alignItems: "center",
-  },
-  userInfoText: {
-    color: COLORS.slate600,
-    fontWeight: "400",
-  },
-  restrictionsTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginBottom: 0,
-  },
-  selectedRestrictions: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    gap: 10,
-  },
-  newRestrictionInput: {
-    flexDirection: "row",
+    container: {
+       
+        borderWidth: 1,
+        marginHorizontal: 20,
+       flexGrow: 1,
+        backgroundColor: "white",
+        borderColor: COLORS.slate200,
+        borderRadius: 15,
+        padding: 20,
+        paddingBottom: 30
+    },
+    profileTopSection: {
+        borderBottomWidth: 1,
+        borderBottomColor: COLORS.slate200,
+        width: "100%",
+        alignItems: "center",
+        paddingVertical: 18,
+        gap: 20,
+    },
+    userName: {
+        fontSize: 24,
+        fontWeight: "bold",
+        textAlign: "center",
+    },
+    userInfoContainer: {
+        marginVertical: 20,
+        gap: 20,
+    },
+    userInfo: {
+        flexDirection: "row",
+        gap: 10,
+        alignItems: "center",
+    },
+    userInfoText: {
+        color: COLORS.slate600,
+        fontWeight: "400",
+    },
+    restrictionsTitle: {
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign: "center",
+        marginBottom: 0,
+    },
+    selectedRestrictions: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        gap: 10,
+    },
+    newRestrictionInput: {
+        flexDirection: "row",
 
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: 20,
-    gap: 8,
-  },
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginTop: 20,
+        gap: 8,
+    },
 });
 
 export default Index;
